13ef236a0f3fca6bc8be03478a04fe364b885ba2
samecrowder <samecrowder@gmail.com>
2018-10-31 04:42:03 +0000

    code cleanup

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
- // res.render('index', { title: 'Express' });
   res.sendFile('babyName.html', { root: 'public/html' });
 });
 
@@ -15,14 +14,6 @@ router.get('/getBoy', function(req, res, next) {
         if (err) throw err;
         var boyList = data.toString().split("\n");
 
-        // var jsonresult = [];
-        // for (var i = 0; i < boyList.length; i++) {
-        //     //console.log(cities[i]);
-        //     if (result != -1) {
-        //         //console.log(cities[i]);
-        //         jsonresult.push({ boyName: boyList[i] });
-        //     }
-        // }
         var jsonresult = boyList[Math.floor(Math.random() * boyList.length)];
         console.log(jsonresult);
         res.status(200).json(jsonresult);
@@ -38,14 +29,6 @@ router.get('/getGirl', function(req, res, next) {
         if (err) throw err;
         var girlList = data.toString().split("\n");
 
-        // var jsonresult = [];
-        // for (var i = 0; i < boyList.length; i++) {
-        //     //console.log(cities[i]);
-        //     if (result != -1) {
-        //         //console.log(cities[i]);
-        //         jsonresult.push({ boyName: boyList[i] });
-        //     }
-        // }
         var jsonresult = girlList[Math.floor(Math.random() * girlList.length)];
         console.log(jsonresult);
         res.status(200).json(jsonresult);
