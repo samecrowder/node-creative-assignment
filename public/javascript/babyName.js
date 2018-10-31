04ab3f2b8534232493f720eaf6c4fc7bf9f6687a
Jared Magnusson <jmag722@gmail.com>
2018-10-31 20:45:38 +0000

    add

--- public/javascript/babyName.js
+++ public/javascript/babyName.js
@@ -2,20 +2,46 @@ var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope, $http) {
     $scope.name = "";
 
-    $scope.getChild = function() {
-        var url = "";
-        if($scope.genderButt === "Male") {
-            url = '/getBoy';
-        
-        }
-        else {
-            url = '/getGirl';
-        }
-        $http.get(url).then(function(response) {
-            console.log(response);
-            $scope.name = response.data;
-        });
+
+    $scope.isMale = function() {
+        document.body.style.backgroundColor = "#89CFF0";
     }
     
+    $scope.isFemale = function() {
+        document.body.style.backgroundColor = "#FFB6C1";
+    }
+    $scope.getChild = function() {
+        var url = "";
+        if ($scope.genderButt === "Male") {
+            url = '/getBoy';
+
+            $http.get(url).then(function(response) {
+                console.log(response);
+                $scope.name = response.data;
+            });
+
+        }
+
+
+
+
+        else if ($scope.genderButt === "Female") {
+            url = '/getGirl';
+
+            $http.get(url).then(function(response) {
+                console.log(response);
+                $scope.name = response.data;
+            });
+        }
+
+        else {
+            $scope.name = "Please choose a gender for your baby."
+        }
+
+
+
+
+    }
+
 
 });
